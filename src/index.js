b41b7d98875e8edd2bc0b332b3a43896c39bed94
dkr-sjr <rkdrjs4637@naver.com>
2025-09-10 11:27:20 +0900

    feat(index.js) : play 함수 구현
    이전에 작성한 compare 함수를 이용하여 play 함수 구현 완료.
    indent depth가 2를 넘기지 않기 위해 함수를 분리해서 작성함.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers){
-    let strike = 0;
-    let ball = 0;
+    let strikeCount = 0;
+    let ballCount = 0;
 
     for(let i = 0 ; i < 3; i+=1){
-      
+      let result = this.compare(computerInputNumbers,userInputNumbers[i], i);
+      if(result === 'strike')
+        strikeCount+=1;
+      else if(result === 'ball')
+        ballCount+=1;
     }
+    
+    let output = '';
+    if(ballCount !== 0)
+      output += (ballCount.toString() + '볼'); 
+    if(output !== '')
+      output += ' ';
+    if(strikeCount !==0)
+      output += (strikeCount.toString() +'스트라이크')
+    if(output ==='')
+      output = '낫싱'
 
-    return "결과 값 String";
+    return output;
   }
 
-  compare(computerInputNumbers, userInputNumber, idx){
-    for(let i =0; i < computerInputNumbers.length ; i+=1){
-      if(computerInputNumbers[i] === userInputNumber && i === idx){
+  compare(computerNumbers, userNumber, idx){
+    for(let i =0; i < computerNumbers.length ; i+=1){
+      if(computerNumbers[i] === userNumber && i === idx){
         return 'strike';
       }
-      if(computerInputNumbers[i] === userInputNumber && i !== idx){
+      if(computerNumbers[i] === userNumber && i !== idx){
         return 'ball';
       }
     }
