3aae0a7e172b05ff339138693b6fad6f29b22440
dkr-sjr <rkdrjs4637@naver.com>
2025-09-10 11:12:27 +0900

    feat(index.js) : compare 함수 구현
    play 함수를 구현하기 위해 그 안에서 사용할 compare 함수를 구현함.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 export default class BaseballGame {
-  constructor(){
-    this.answer = [0,0,0];
-    this.initialization();
-  }
-  
-  initialization(){
-    for(let i =0 ; i < 3; i+=1)
-      this.answer[i] = MissionUtils.Random.pickNumberInRange(1,9);
-  }
-  
-  play(computerInputNumbers, userInputNumbers) {
+  play(computerInputNumbers, userInputNumbers){
+    let strike = 0;
+    let ball = 0;
+
+    for(let i = 0 ; i < 3; i+=1){
+      
+    }
+
     return "결과 값 String";
+  }
+
+  compare(computerInputNumbers, userInputNumber, idx){
+    for(let i =0; i < computerInputNumbers.length ; i+=1){
+      if(computerInputNumbers[i] === userInputNumber && i === idx){
+        return 'strike';
+      }
+      if(computerInputNumbers[i] === userInputNumber && i !== idx){
+        return 'ball';
+      }
+    }
+    return 'Nothing';
   }
 }
